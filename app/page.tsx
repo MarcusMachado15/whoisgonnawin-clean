a7f92bd594d10689681ff0e203afd6719f53c557
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-05-29 10:50:50 +0700

    Atualiza classificação Grupo C África

--- app/page.tsx
+++ app/page.tsx
@@ -861,7 +861,7 @@ const groupMatchesAsia: Record<string, Match[]> = {
     { home: '🇺🇿 Uzbekistan',   score: '1 - 1', away: '🇱🇧 Lebanon'     },
     { home: '🇸🇦 Saudi Arabia', score: '0 - 1', away: '🇻🇳 Vietnam'     },
     { home: '🇻🇳 Vietnam',      score: '0 - 1', away: '🇺🇿 Uzbekistan' },
-    { home: '🇧🇩 Bangladesh',   score: '', away: '🇸🇦 Saudi Arabia'},
+    { home: '🇧🇩 Bangladesh',   score: '0 - 0', away: '🇸🇦 Saudi Arabia'},
     { home: '🇺🇿 Uzbekistan',   score: '', away: '🇧🇩 Bangladesh' },
     { home: '🇱🇧 Lebanon',     score: '', away: '🇻🇳 Vietnam'     },
     { home: '🇸🇦 Saudi Arabia', score: '', away: '🇺🇿 Uzbekistan' },
