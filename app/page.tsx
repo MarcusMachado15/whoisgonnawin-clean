d949f05a8ab2add131a0b89199416cba34d5d2bc
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-06-01 21:58:14 +0700

    Atualiza classificação – Grupo A NC America com 4 jogos

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ const groupMatchesEurope: Record<string, Match[]> = {
     { home: '🇩🇰 Denmark', score: '0 – 0', away: '🇨🇿 Czech Rep.' },
     { home: '🇨🇿 Czech Rep.', score: '1 - 2', away: '🇧🇪 Belgium' },
     { home: '🇰🇿 Kazakhstan', score: '3 - 2', away: '🇩🇰 Denmark' },
-    { home: '🇧🇪 Belgium', score: '', away: '🇰🇿 Kazakhstan' },
+    { home: '🇧🇪 Belgium', score: '0 - 0', away: '🇰🇿 Kazakhstan' },
     { home: '🇸🇮 Slovenia', score: '', away: '🇨🇿 Czech Rep.' },
     { home: '🇩🇰 Denmark', score: '', away: '🇧🇪 Belgium' },
     { home: '🇰🇿 Kazakhstan', score: '', away: '🇸🇮 Slovenia' },
