61362c7101fbc316de989aadd08b0240928f552f
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-06-04 11:57:05 +0700

    Atualiza classificação dos grupos D e E (6 jogos)

--- app/page.tsx
+++ app/page.tsx
@@ -641,18 +641,18 @@ const groupMatchesSouthAmerica: Record<string, Match[]> = {
 };
 const groupStandingsSouthAmerica: Record<string, Standing[]> = {
     'Group A WC Qualifiers Argentina 2025': [
-    { pos: 1, team: 'Argentina', played: 2, won: 1, draw: 1, lost: 0, gf: 4, ga: 2, gd:  2, pts: 4 },
-    { pos: 2, team: 'Ecuador',   played: 2, won: 1, draw: 1, lost: 0, gf: 5, ga: 4, gd:  1, pts: 4 },
-    { pos: 3, team: 'Colombia',  played: 2, won: 1, draw: 0, lost: 1, gf: 5, ga: 3, gd:  2, pts: 3 },
-    { pos: 4, team: 'Paraguay',  played: 1, won: 0, draw: 0, lost: 1, gf: 0, ga: 2, gd: -2, pts: 0 },
-    { pos: 5, team: 'Chile',     played: 1, won: 0, draw: 0, lost: 1, gf: 0, ga: 3, gd: -3, pts: 0 },
+    { pos: 1, team: 'Ecuador',    played: 3, won: 2, draw: 1, lost: 0, gf: 6,  ga: 4,  gd: +2, pts: 7 },
+    { pos: 2, team: 'Argentina',  played: 3, won: 1, draw: 2, lost: 0, gf: 5,  ga: 3,  gd: +2, pts: 5 },
+    { pos: 3, team: 'Colombia',   played: 2, won: 1, draw: 0, lost: 1, gf: 5,  ga: 3,  gd: +2, pts: 3 },
+    { pos: 4, team: 'Chile',      played: 2, won: 0, draw: 1, lost: 1, gf: 1,  ga: 4,  gd: -3, pts: 1 },
+    { pos: 5, team: 'Paraguay',   played: 2, won: 0, draw: 0, lost: 2, gf: 0,  ga: 3,  gd: -3, pts: 0 },
   ],
   'Group B WC Qualifiers Argentina 2025': [
-    { pos: 1, team: 'Uruguay',   played: 2, won: 2, draw: 0, lost: 0, gf: 6, ga: 2, gd:  4, pts: 6 },
-    { pos: 2, team: 'Peru',      played: 2, won: 1, draw: 0, lost: 1, gf: 3, ga: 3, gd:  0, pts: 3 },
-    { pos: 3, team: 'Venezuela', played: 1, won: 0, draw: 1, lost: 0, gf: 1, ga: 1, gd:  0, pts: 1 },
-    { pos: 4, team: 'Brazil',    played: 2, won: 0, draw: 1, lost: 1, gf: 2, ga: 4, gd: -2, pts: 1 },
-    { pos: 5, team: 'Bolivia',   played: 1, won: 0, draw: 0, lost: 1, gf: 2, ga: 3, gd: -1, pts: 0 },
+    { pos: 1, team: 'Uruguay',    played: 2, won: 2, draw: 0, lost: 0, gf: 6,  ga: 2,  gd: +4, pts: 6 },
+    { pos: 2, team: 'Brazil',     played: 3, won: 1, draw: 1, lost: 1, gf: 6,  ga: 5,  gd: +1, pts: 4 },
+    { pos: 3, team: 'Peru',       played: 3, won: 1, draw: 1, lost: 1, gf: 3,  ga: 4,  gd: -1, pts: 4 },
+    { pos: 4, team: 'Venezuela',  played: 2, won: 0, draw: 2, lost: 0, gf: 1,  ga: 1,  gd:  0, pts: 2 },
+    { pos: 5, team: 'Bolivia',    played: 2, won: 0, draw: 0, lost: 2, gf: 3,  ga: 7,  gd: -4, pts: 0 },
   ],
   'Group A WC Qualifiers Turkey 2024': [
   { pos: 1, team: 'Argentina(Q)', played: 4, won: 2, draw: 1, lost: 1, gf:  9, ga:  6, gd:  3, pts:  7 },
