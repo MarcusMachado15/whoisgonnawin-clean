ecd4626f007d9bc7ba98a192f962746e0b4fb5c6
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-08-07 23:27:05 +0700

    Atualizei classificação do Grupo B com jogos 7 e 8

--- app/page.tsx
+++ app/page.tsx
@@ -719,7 +719,7 @@ const groupMatchesAfrica: Record<string, Match[]> = {
     { home: '🇿🇼 Zimbabwe',     score: '2 - 0', away: '🇹🇳 Tunisia'     },
     { home: '🇸🇳 Senegal',      score: '1 - 1', away: '🇿🇼 Zimbabwe'    },
     { home: '🇬🇳 Guinea',       score: '1 - 1', away: '🇿🇦 South Africa' },
-    { home: '🇹🇳 Tunisia',      score: '', away: '🇸🇳 Senegal'     },
+    { home: '🇹🇳 Tunisia',      score: '2 - 2', away: '🇸🇳 Senegal'     },
     { home: '🇿🇼 Zimbabwe',     score: '', away: '🇬🇳 Guinea'       },
     { home: '🇬🇳 Guinea',       score: '', away: '🇹🇳 Tunisia'     },
     { home: '🇿🇦 South Africa', score: '', away: '🇿🇼 Zimbabwe'    },
