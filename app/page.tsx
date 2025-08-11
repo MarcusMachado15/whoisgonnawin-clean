ea1d99c6db3ddb9a64f591bbfefe0db9f23f8db8
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-08-11 21:37:32 +0700

    Atualiza ranking do Grupo E (Ásia) com os jogos 7 e 8

--- app/page.tsx
+++ app/page.tsx
@@ -351,12 +351,12 @@ const groupMatchesEurope: Record<string, Match[]> = {
 
 const groupStandingsEurope: Record<string, Standing[]> = {
   'Group A WC Qualifiers Argentina 2025': [
-  { pos: 1, team: 'Spain',      played: 4, won: 2, draw: 2, lost: 0, gf: 9, ga: 7, gd: +2, pts: 8 },
-  { pos: 2, team: 'Wales',      played: 3, won: 2, draw: 1, lost: 0, gf: 8, ga: 3, gd: +5, pts: 7 },
-  { pos: 3, team: 'Finland',    played: 3, won: 1, draw: 2, lost: 0, gf: 5, ga: 3, gd: +2, pts: 5 },
-  { pos: 4, team: 'Serbia',     played: 3, won: 0, draw: 1, lost: 2, gf: 4, ga: 9, gd: -5, pts: 1 },
-  { pos: 5, team: 'Montenegro', played: 3, won: 0, draw: 0, lost: 3, gf: 5, ga: 9, gd: -4, pts: 0 },
-],
+  { pos: 1, team: 'Wales',       played: 4, won: 2, draw: 2, lost: 0, gf: 10, ga: 5,  gd: +5, pts: 8 },
+  { pos: 2, team: 'Spain',       played: 4, won: 2, draw: 2, lost: 0, gf:  9, ga: 7,  gd: +2, pts: 8 },
+  { pos: 3, team: 'Finland',     played: 4, won: 1, draw: 3, lost: 0, gf:  7, ga: 5,  gd: +2, pts: 6 },
+  { pos: 4, team: 'Montenegro',  played: 4, won: 1, draw: 0, lost: 3, gf:  7, ga:10,  gd: -3, pts: 3 },
+  { pos: 5, team: 'Serbia',      played: 4, won: 0, draw: 1, lost: 3, gf:  5, ga:11,  gd: -6, pts: 1 },
+]
   'Group B WC Qualifiers Argentina 2025': [
   { pos: 1, team: 'France',     played: 4, won: 4, draw: 0, lost: 0, gf: 10, ga: 1,  gd: +9, pts: 12 },
   { pos: 2, team: 'Sweden',     played: 3, won: 2, draw: 0, lost: 1, gf:  8, ga: 4,  gd: +4, pts:  6 },
