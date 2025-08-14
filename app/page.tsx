65dbaf2302895b9f30cb8fb511170233fa33d8f6
MarcusMachado15 <marquinhosmv15@hotmail.com>
2025-08-14 14:45:37 +0700

    Europe Group H: standings updated after matches 9 & 10

--- app/page.tsx
+++ app/page.tsx
@@ -564,7 +564,7 @@ const groupMatchesSouthAmerica: Record<string, Match[]> = {
     { home: '🇻🇪 Venezuela', score: '0 - 0', away: '🇵🇪 Peru' },
     { home: '🇺🇾 Uruguay',   score: '3 - 0', away: '🇧🇷 Brazil' },
     { home: '🇧🇴 Bolivia',   score: '3 - 1', away: '🇻🇪 Venezuela' },
-    { home: '🇻🇪 Venezuela', score: '', away: '🇺🇾 Uruguay' },
+    { home: '🇻🇪 Venezuela', score: '1 - 0', away: '🇺🇾 Uruguay' },
     { home: '🇵🇪 Peru',      score: '', away: '🇧🇴 Bolivia' },
   ],'Group A WC Qualifiers Turkey 2024': [
   { home: '🇦🇷 Argentina',   score: '1 – 2', away: '🇨🇱 Chile'     },
